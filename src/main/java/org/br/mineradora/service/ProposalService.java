9b879da53c9e0b541ab121b917464ca3dba86b41
gmozart <mozart.gleison@gmail.com>
2023-07-07 11:48:45 -0300

    Implementando interface e metodos para a manipulação dos dados da Proposta.

---
+++ src/main/java/org/br/mineradora/service/ProposalService.java
@@ -0,0 +1,17 @@
+package org.br.mineradora.service;
+
+import org.br.mineradora.dto.ProposalDetailsDTO;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public interface ProposalService {
+
+    ProposalDetailsDTO findFullProposal(long id);
+
+    void createNewProposal(ProposalDetailsDTO proposalDetailsDTO);
+
+    void removeProposal(long id);
+
+
+}
